d319cd019d845c18c046d2908f15f6fce677e951
dnbias <daniel.biasiotto@edu.unito.it>
2023-12-07 13:42:36 +0100

    merging

--- hroom-server/src/main/java/com/dnbias/hroom/repository/FeedbackRepositoy.java
+++
@@ -1,9 +0,0 @@
-package com.dnbias.hroom.repository;
-
-import com.dnbias.hroom.feedback.Feedback;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FeedbackRepositoy extends JpaRepository<Feedback, Long> {
-}
