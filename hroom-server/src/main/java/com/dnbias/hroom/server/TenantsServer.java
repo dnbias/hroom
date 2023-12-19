47911abb2a6ae0f241f49075f1231c14a97ebcca
dnbias <daniel.biasiotto@edu.unito.it>
2023-12-19 08:19:30 +0100

    first eureka configuration

--- hroom-server/src/main/java/com/dnbias/hroom/server/TenantsServer.java
+++ hroom-server/src/main/java/com/dnbias/hroom/server/TenantsServer.java
@@ -5,10 +5,18 @@ import com.dnbias.hroom.repository.TenantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableAutoConfiguration
 @EnableDiscoveryClient
+@SpringBootApplication(scanBasePackages="com.dnbias.hroom")
+@EnableJpaRepositories("com.dnbias.hroom.*")
+@ComponentScan(basePackages = { "com.dnbias.hroom.*" })
+@EntityScan("com.dnbias.hroom.*")
 public class TenantsServer {
 
     @Autowired
