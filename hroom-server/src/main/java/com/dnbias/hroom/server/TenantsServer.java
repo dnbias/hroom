06004c3d3ee9d8d32732554bc6377d5cf0e41b5a
dnbias <daniel.biasiotto@edu.unito.it>
2023-12-22 07:05:05 +0100

    fixes and docker-compose update

--- hroom-server/src/main/java/com/dnbias/hroom/server/TenantsServer.java
+++ hroom-server/src/main/java/com/dnbias/hroom/server/TenantsServer.java
@@ -8,22 +8,36 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
-@EnableAutoConfiguration
+// @EnableAutoConfiguration
 @EnableDiscoveryClient
-@SpringBootApplication(scanBasePackages="com.dnbias.hroom")
-@EnableJpaRepositories("com.dnbias.hroom.*")
-@ComponentScan(basePackages = { "com.dnbias.hroom.*" })
-@EntityScan("com.dnbias.hroom.*")
+@SpringBootApplication(scanBasePackageClasses = {
+        com.dnbias.hroom.user.Tenant.class,
+        com.dnbias.hroom.service.TenantService.class,
+        com.dnbias.hroom.service.TenantServiceImpl.class,
+        com.dnbias.hroom.repository.TenantRepository.class,
+        com.dnbias.hroom.controller.TenantController.class
+    }, scanBasePackages = "com.dnbias.hroom.controller.repository")
+// @ComponentScan(basePackageClasses = com.dnbias.hroom.user.Tenant.class)
+// @ComponentScan(basePackageClasses = com.dnbias.hroom.service.TenantService.class)
+// @ComponentScan(basePackageClasses = com.dnbias.hroom.service.TenantServiceImpl.class)
+// @ComponentScan(basePackageClasses = com.dnbias.hroom.repository.TenantRepository.class)
+// @ComponentScan(basePackageClasses = com.dnbias.hroom.controller.TenantController.class)
+// @EnableJpaRepositories("com.dnbias.hroom.*")
+// @ComponentScan(basePackages = { "com.dnbias.hroom.*" })
+// @EntityScan("com.dnbias.hroom.*")
 public class TenantsServer {
 
-    @Autowired
-    TenantRepository tenantRepository;
+    // @Autowired
+    // TenantRepository tenantRepository;
 
     public static void main(String[] args) {
         System.setProperty("spring.config.name", "tenants-server");
         SpringApplication.run(TenantsServer.class, args);
     }
+
 }
