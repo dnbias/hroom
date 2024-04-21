fb5d466670e4d211141814ee4003d7d13c11ac7c
dnbias <daniel.biasiotto@edu.unito.it>
2024-04-21 02:08:37 +0200

    fixes to docker & properties

--- hroom-server/landlord-service/src/main/java/com/hroom/landlord/LandlordServiceApplication.java
+++ hroom-server/landlord-service/src/main/java/com/hroom/landlord/LandlordServiceApplication.java
@@ -2,10 +2,8 @@ package com.hroom.landlord;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class LandlordServiceApplication {
 
     public static void main(String[] args) {
--- hroom-server/reservation-service/src/main/java/com/hroom/reservation/ReservationServiceApplication.java
+++ hroom-server/reservation-service/src/main/java/com/hroom/reservation/ReservationServiceApplication.java
@@ -2,10 +2,8 @@ package com.hroom.reservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class ReservationServiceApplication {
 
     public static void main(String[] args) {
